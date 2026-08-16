51fb3270075fecce16d723b5ff832ae1295b1a08
Shehan Samaranayaka <spsamaranayaka@students.nsbm.ac.lk>
2026-08-16 15:34:48 +0530

    fixed connection-string issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://samaranayaka2004_db_user:ObOtpebaqhXqvmHy@ac-ktbzidj-shard-00-00.oxpiewx.mongodb.net:27017,ac-ktbzidj-shard-00-01.oxpiewx.mongodb.net:27017,ac-ktbzidj-shard-00-02.oxpiewx.mongodb.net:27017/?ssl=true&replicaSet=atlas-kev7qk-shard-0&authSource=admin&appName=Cluster0");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected successfully");
     }catch (error) {
         console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
     }
 };
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import { connectDB } from '../config/db.js';
 import notesRoutes from './routes/notesRoutes.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
 });
 
 //mongodb+srv://samaranayaka2004_db_user:954x4R5aAJgShNZF@cluster0.weugpqu.mongodb.net/?appName=Cluster0
