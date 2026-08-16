851eb261477773335a7ed8b57f6e815dd5b9fc5c
Shehan Samaranayaka <spsamaranayaka@students.nsbm.ac.lk>
2026-08-16 09:36:28 +0530

    Database connected successfully

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://samaranayaka2004_db_user:ObOtpebaqhXqvmHy@cluster0.oxpiewx.mongodb.net/?appName=Cluster0");
+        await mongoose.connect("mongodb://samaranayaka2004_db_user:ObOtpebaqhXqvmHy@ac-ktbzidj-shard-00-00.oxpiewx.mongodb.net:27017,ac-ktbzidj-shard-00-01.oxpiewx.mongodb.net:27017,ac-ktbzidj-shard-00-02.oxpiewx.mongodb.net:27017/?ssl=true&replicaSet=atlas-kev7qk-shard-0&authSource=admin&appName=Cluster0");
         console.log("MongoDB connected successfully");
     }catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
 };
 
-// mongodb+srv://samaranayaka2004_db_user:ObOtpebaqhXqvmHy@cluster0.oxpiewx.mongodb.net/?appName=Cluster0
+
+//mongodb://samaranayaka2004_db_user:<db_password>@ac-ktbzidj-shard-00-00.oxpiewx.mongodb.net:27017,ac-ktbzidj-shard-00-01.oxpiewx.mongodb.net:27017,ac-ktbzidj-shard-00-02.oxpiewx.mongodb.net:27017/?ssl=true&replicaSet=atlas-kev7qk-shard-0&authSource=admin&appName=Cluster0
